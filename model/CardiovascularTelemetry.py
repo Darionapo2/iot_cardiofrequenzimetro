cfd59525be4e2b964509bc4d6efe3c80a8425999
Dario <darionapo2.0@gmail.com>
2024-02-13 22:01:13 +0100

    Implemented main.py.

--- model/CardiovascularTelemetry.py
+++ model/CardiovascularTelemetry.py
@@ -1,10 +1,14 @@
 import json
 
 class CardiovascularTelemetry:
-    heart_rate: float # [bpm] beats per minute
+    heart_rate: int # [bpm] beats per minute
     oxygen_saturation: float # % of hemoglobin saturation
 
-    def __init__(self, heart_rate: float, oxygen_saturation: float) -> None:
+    def __init__(self, heart_rate: int, oxygen_saturation: float) -> None:
+        self.heart_rate = heart_rate
+        self.oxygen_saturation = oxygen_saturation
+
+    def update_values(self, heart_rate: int, oxygen_saturation: float) -> None:
         self.heart_rate = heart_rate
         self.oxygen_saturation = oxygen_saturation
 
---
+++ process/CMWSimulator.py
@@ -0,0 +1,10 @@
+import random
+
+class CMWSimulator:
+
+
+
+    def simulate_sensors_record(self):
+        new_heart_rate = random.randint(65, 110)
+        new_oxygen_saturation = random.uniform(95, 100)
+
